dd152666681f619ebc5c323d3a27ef9703212252
Erico Silva <augusto.ericosilva@gmail.com>
2017-02-22 20:26:37 -0300

    Add style to examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
+
 
 var Examples = (props) => {
   return (
       <div>
-        <h3>Examples</h3>
-        <p>Welcome to examples page!</p>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few examples locations to try out:</p>
+        <ul>
+            <li>
+                <Link to='/?location=mairipora'>Mairipora, SP</Link>
+            </li>
+            <li>
+                <Link to='/?location=sao+paulo'>Sao Paulo, SP</Link>
+            </li>
+        </ul>
       </div>
   )
 };
